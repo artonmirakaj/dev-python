48d43fa16e581edf9335bd04349c4d390b5b08fb
Arton Mirakaj <artonmirakaj@Arton.lan>
2022-07-08 13:55:32 -0400

    Initial commit

---
+++ Numbers-Math-Operators/script.py
@@ -0,0 +1,20 @@
+# Numbers - int & float
+print(2 + 2)
+num = 2
+decimal = 3.14159265
+print(type(num))
+print(type(decimal))
+print(type(20 + 1.5))
+print(type(9.9 + 1.1) and 9.9 + 1.1)
+
+print(2 ** 3)
+
+# returns int rounded down
+print(5 // 4)
+
+# modulo
+print(5 % 4 and type(5 % 4))
+
+print(round(3.9))
+
+print(abs(-20))
